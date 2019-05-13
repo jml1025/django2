f587b83989a9c126946ab1549928bbb2fad34478
jml1025 <jmlee1025@naver.com>
2019-05-13 22:52:18 +0900

    news app

--- mysite/newsapp/urls.py
+++ mysite/newsapp/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
-    path('articles/',include('newsapp.urls'))
+    path('<int:year>/', views.year_archive),
+    path('<int:year>/<int:month>/', views.month_archive),
+    path('<int:year>/<int:month>/<int:pk>/', views.article_detail),
 ]
